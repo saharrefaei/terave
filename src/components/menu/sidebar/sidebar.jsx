f520d083c7be3b507d24500a4017b58860b755a5
Your Correct Username <saharrefaei1999@gmail.com>
2024-02-01 16:46:48 +0100

    bug : resulving bug for building

--- src/components/menu/sidebar/sidebar.jsx
+++ src/components/menu/sidebar/sidebar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import Links from "../Links/Link";
 import "./sideBar.scss";
+// i change the side bar
 export default function SideBar({ open, setOpen }) {
   const variants = {
     open: {
